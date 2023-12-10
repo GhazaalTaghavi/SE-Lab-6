249b75d6182fe847ae8784afb49ee79d37d1d326
GhazaalTaghavi <ghazaltgi@gmail.com>
2023-12-10 15:03:21 +0330

    Nesting

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -22,17 +22,13 @@ public class Parser {
     public Parser() {
         parsStack = new Stack<Integer>();
         parsStack.push(0);
-        try {
-            parseTable = new ParseTable(Files.readAllLines(Paths.get("src/main/resources/parseTable")).get(0));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         rules = new ArrayList<Rule>();
         try {
+            parseTable = new ParseTable(Files.readAllLines(Paths.get("src/main/resources/parseTable")).get(0));
             for (String stringRule : Files.readAllLines(Paths.get("src/main/resources/Rules"))) {
                 rules.add(new Rule(stringRule));
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         cg = new CodeGenerator();
@@ -46,10 +42,8 @@ public class Parser {
         while (!finish) {
             try {
                 Log.print(/*"lookahead : "+*/ lookAhead.toString() + "\t" + parsStack.peek());
-//                Log.print("state : "+ parsStack.peek());
                 currentAction = parseTable.getActionTable(parsStack.peek(), lookAhead);
                 Log.print(currentAction.toString());
-                //Log.print("");
 
                 switch (currentAction.action) {
                     case shift:
@@ -58,21 +52,7 @@ public class Parser {
 
                         break;
                     case reduce:
-                        Rule rule = rules.get(currentAction.number);
-                        for (int i = 0; i < rule.RHS.size(); i++) {
-                            parsStack.pop();
-                        }
-
-                        Log.print(/*"state : " +*/ parsStack.peek() + "\t" + rule.LHS);
-//                        Log.print("LHS : "+rule.LHS);
-                        parsStack.push(parseTable.getGotoTable(parsStack.peek(), rule.LHS));
-                        Log.print(/*"new State : " + */parsStack.peek() + "");
-//                        Log.print("");
-                        try {
-                            cg.semanticFunction(rule.semanticAction, lookAhead);
-                        } catch (Exception e) {
-                            Log.print("Code Genetator Error");
-                        }
+                        handleReduceAction(currentAction, lookAhead);
                         break;
                     case accept:
                         finish = true;
@@ -99,5 +79,24 @@ public class Parser {
             }
         }
         if (!ErrorHandler.hasError) cg.printMemory();
+    }
+
+    private void handleReduceAction(Action currentAction, Token lookAhead) {
+        Rule rule = rules.get(currentAction.number);
+
+        for (int i = 0; i < rule.RHS.size(); i++) {
+            parsStack.pop();
+        }
+
+        parsStack.push(parseTable.getGotoTable(parsStack.peek(), rule.LHS));
+
+        try {
+            cg.semanticFunction(rule.semanticAction, lookAhead);
+        } catch (Exception e) {
+            Log.print("Code Generator Error");
+        }
+
+        Log.print(parsStack.peek() + "\t" + rule.LHS);
+        Log.print(parsStack.peek() + "");
     }
 }
